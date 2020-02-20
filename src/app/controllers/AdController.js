2d80882a89db5b6db1cdf8e6b6e63728f065b898
= MaykonMorais <maykon.estudos@gmail.com>
2020-02-20 20:21:29 -0300

    pagination on Ads

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -3,7 +3,12 @@ const Ad = require('../models/Ad')
 class AdController {
   // list all
   async index(req, res) {
-    const ads = await Ad.find()
+    const ads = await Ad.paginate({}, {
+      page: req.query.page || 1,
+      populate: ['author'],
+      limit: 20,
+      sort: '-createdAt'
+    })
 
     return res.json(ads);
   }
